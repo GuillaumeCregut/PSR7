03fea1866e9981e727d56af64e1d50dba08d1753
Guillaume Crégut <gcregut@free.fr>
2025-03-04 11:45:36 +0100

    renaming testFile, adding tests for headers

---
+++ tests/AbstractMessageTest.php
@@ -0,0 +1,90 @@
+<?php
+
+use App\Implementation\AbstractMessage;
+use App\Implementation\Request;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(Request::class)]
+class AbstractMessageTest extends TestCase
+{
+    /* Nécessite une implémentation plus poussée du setting des headers */
+    public function testGetHeaders()
+    {
+        $request = new Request([]);
+        $this->assertIsArray($request->getHeaders());
+    }
+
+    public function testHasHeaderWithNoHeaders()
+    {
+        $request = new Request([]);
+        $this->assertFalse($request->hasHeader('test'));
+    }
+
+    public function testHasHeader()
+    {
+        $headers['test']=array('valeur1', 'valeur2');
+        $headers['titi']=array('valeur1', 'valeur2');
+        $request = new Request($headers);
+        $this->assertTrue($request->hasHeader('test'));
+        $this->assertTrue($request->hasHeader('Test'));
+        $this->assertFalse($request->hasHeader('toto'));
+    }
+
+    public function testGetheaderWithNoHeader()
+    {
+        $request = new Request([]);
+        $this->assertIsArray($request->getHeader('toto'));
+        $this->assertEquals(0, count($request->getHeader('toto')));
+    }
+
+    public function testGetHeader()
+    {
+        $headers['test']=array('valeur1', 'valeur2');
+        $headers['titi']=array('valeur3', 'valeur4');
+        $request = new Request($headers);
+        $result = $request->getHeader('test');
+        $this->assertIsArray($result);
+        $this->assertEquals( $headers['test'], $result);
+    }
+
+    public function testSettingsHeadersWithIllegalCharInKey()
+    {
+        $badKey1 = "Hello\0world";
+        $badKey2 = "Hello\rworld";
+        $badKey3 = "Hello\nworld";
+        $badKey4 = "Hel\0lo\nworld\r";
+        $badKey5 = "Hello". chr(19) ."world";
+        $badKey6 = "Hello world";
+        $headers[$badKey1]= array('value1', 'value2');
+        $headers[$badKey2]= array('value1', 'value2');
+        $headers[$badKey3]= array('value1', 'value2');
+        $headers[$badKey4]= array('value1', 'value2');
+        $headers[$badKey5]= array('value1', 'value2');
+        $headers[$badKey6]= array('value1', 'value2');
+        $request = new Request($headers);
+        $this->assertArrayNotHasKey($badKey1,$request->getHeaders());
+        $this->assertArrayNotHasKey($badKey2,$request->getHeaders());
+        $this->assertArrayNotHasKey($badKey3,$request->getHeaders());
+        $this->assertArrayNotHasKey($badKey4,$request->getHeaders());
+        $this->assertArrayNotHasKey($badKey5,$request->getHeaders());
+        $this->assertArrayNotHasKey($badKey6,$request->getHeaders());
+    }
+
+    public function testHeadersWithIllegalCharInValues()
+    {
+        $badKey1 = "test1";
+        $badKey2 = "test2";
+        $badKey3 = "test3";
+        $goodKey = "test4";
+        $headers[$badKey1]= array("value1 \n", 'value2');
+        $headers[$badKey2]= array('value1', "value2 \r");
+        $headers[$badKey3]= array("value1 \0", 'value2');
+        $headers[$goodKey]= array("value1 ", 'value2');
+        $request = new Request($headers);
+        $this->assertArrayNotHasKey($badKey1,$request->getHeaders());
+        $this->assertArrayNotHasKey($badKey2,$request->getHeaders());
+        $this->assertArrayNotHasKey($badKey3,$request->getHeaders());
+        $this->assertArrayHasKey($goodKey, $request->getHeaders());
+    }
+}
