1c6c61318fc203f9ce4c7e455733755573ae44d1
Guillaume Crégut <gcregut@free.fr>
2025-03-04 11:46:34 +0100

    creating abstract class for message

---
+++ src/Implementation/AbstractMessage.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Implementation;
+
+use App\Interfaces\MessageInterface;
+
+abstract class AbstractMessage implements MessageInterface
+{
+    protected array $headers =[];
+    
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
+
+    public function hasHeader(string $name): bool
+    {
+        if (count($this->headers)===0) {
+            return false;
+        }
+        $result = $this->getHeaderKey($name);
+        return ($result !== null);
+    }
+
+    public function getHeader(string $name): array
+    {
+        $key=$this->getHeaderKey($name);
+        if(is_null($key)) {
+            return [];
+        }
+        return $this->headers[$key];
+    }
+
+    protected function setHeaders(array $headers): static
+    {
+        foreach($headers as $key=>$value) {
+            if ($this->testKey($key)) {
+                if($this->testValue($value)) {
+                    $this->headers[$key]=$value;
+                }
+            }
+        }
+        return $this;
+    }
+
+    private function getHeaderKey(string $name): string | null
+    {
+        $result = array_find_key($this->headers, function(array $value, string $key) use ($name) {
+            return strtoupper($name) === strtoupper($key);
+        });
+        return $result;
+    }
+
+    private function testKey(string $key): bool
+    {
+        for ($i = 0; $i < strlen($key); $i++) {
+            if (ord($key[$i]) <= 0x20) {
+                return false; 
+            }
+        }
+        return true;
+    }
+
+    private function testValue(array $values): bool
+    {
+        foreach($values as $value) {
+            if((str_contains($value, "\0") || str_contains($value, "\r") || str_contains($value, "\n")))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
