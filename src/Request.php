5de743c175ea64d4d6fb163a8b703ff0ee59532c
Guillaume Crégut <gcregut@free.fr>
2025-03-06 14:12:38 +0100

    move implementations classes

---
+++ src/Request.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+
+use App\Interfaces\RequestInterface;
+use App\Implementation\AbstractRequest;
+
+
+class Request extends AbstractRequest implements RequestInterface
+{
+   
+   
+   
+
+    
+}
